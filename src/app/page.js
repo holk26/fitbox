ceea52de5489c1aed8dba780c6cd7b98f18c1b6e
Homero Cabrera Araque <hcabreraa@ufinet.com>
2024-06-19 22:28:08 -0500

    ✨ NEW:

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 "use client"
 import { DashBoard } from "@/components/component/dash-board";
+import Head from "next/head";
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>FitBox</title>
+    </Head>
     <DashBoard />
+    </>
   );
 }
